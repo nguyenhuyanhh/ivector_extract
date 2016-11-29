8cc5f0cbe9eeba0148110bcda10cc9fa06b48280
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2016-11-29 18:56:28 +0800

    use sph as input (WIP)

--- client.sh
+++ client.sh
@@ -2,21 +2,26 @@
 
 . path.sh
 
-cmd=run.pl
-
 data="./workflow/data"
+conf="./workflow/conf"
 temp="./workflow/temp"
 result="./workflow/result"
 log="./workflow/log"
 
-feats="ark,ns,cs:add-deltas scp:$data/feats.scp ark:- | apply-cmvn-sliding --norm-vars=false --center=true --cmn-window=300 ark:- ark:- | select-voiced-frames ark:- scp,ns,cs:$data/vad.scp ark:- |"
+run.pl $log/mfcc.log compute-mfcc-feats  --verbose=2 --config=$conf/mfcc.conf scp:$data/wav.scp ark:- \| \
+    copy-feats --compress=false ark:- ark,scp,t:$temp/mfcc.ark,$temp/mfcc.scp || exit 1;
+
+echo "Computed mfcc"
+
+run.pl $log/vad.log compute-vad --config=$conf/vad.conf scp:$temp/mfcc.scp ark,scp,t:$temp/vad.ark,$temp/vad.scp || exit 1;
+
+echo "Computed vad"
+
+feats="ark,ns,cs:add-deltas scp:$temp/mfcc.scp ark:- | apply-cmvn-sliding --norm-vars=false --center=true --cmn-window=300 ark:- ark:- | select-voiced-frames ark:- scp,ns,cs:$temp/vad.scp ark:- |"
 
 ivector-extract-client "$feats"
 
-cat $temp/ivector.temp.scp > $result/ivector.scp
-cp $temp/ivector.temp.ark $result/ivector.ark
-
-$cmd $log/speaker_mean.log \
+run.pl $log/speaker_mean.log \
     ivector-normalize-length scp:$result/ivector.scp  ark:- \| \
     ivector-mean ark:$data/spk2utt ark:- ark:- ark,t:$result/num_utts.ark \| \
     ivector-normalize-length ark:- ark,scp:$result/spk_ivector.ark,$result/spk_ivector.scp 
--- server.sh
+++ server.sh
@@ -7,13 +7,13 @@ min_post=0.025 # Minimum posterior to use (posteriors below this are pruned out)
 
 models="./workflow/models"
 data="./workflow/data"
-temp="./workflow/temp"
+result="./workflow/result"
 
-# gmm="fgmm-global-to-gmm $models/final.ubm -|"
+# gmm="fgmm-global-to-gmm $models/final.ubm -|" # if there is no final.dubm
 gmm="$models/final.dubm"
 fgmm="$models/final.ubm"
 ivector="$models/final.ie"
 
-tmp="ark,scp,t:$temp/ivector.temp.ark,$temp/ivector.temp.scp"
+results="ark,scp,t:$result/ivector.ark,$result/ivector.scp"
 
-ivector-extract-server --verbose=2 --n=$num_gselect --min-post=$min_post "$gmm" "$fgmm" "$ivector" "$tmp" || exit 1;
+ivector-extract-server --verbose=2 --n=$num_gselect --min-post=$min_post "$gmm" "$fgmm" "$ivector" "$results" || exit 1;
